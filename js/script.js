7485726abaa4b10a2e772208850ceab29cf684a1
ShriiiTrackingSolutions <shriitrackingsolutions86@gmail.com>
2025-06-07 12:57:18 +0530

    page completed

--- js/script.js
+++ js/script.js
@@ -12,61 +12,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Load Header and Footer
 function loadHeaderFooter(callback) {
-  document.getElementById("header").innerHTML = ` <div class="miniHeader d-none d-sm-block">
-            <div class="container">
-
-            
-<div class="row">
-  <div class="col-auto topheadLinks">
-    <a target="_blank" href="tel:yourNumber"><i class="fa-solid fa-phone" aria-hidden="true"></i> YourNumber</a>
-    <a target="_blank" href="#"><i class="fa-solid fa-location-dot" aria-hidden="true"></i>Vadodara</a>
-  </div>
-  <div class="col-auto topheadSocial">
-    <a href=""><i class="fa-brands fa-facebook"></i></a>
-    <a href=""><i class="fa-brands fa-square-instagram"></i></a>
-    <a href=""><i class="fa-brands fa-linkedin"></i></a>
-  </div>
-</div>
-            </div>
-          </div>
-        <div id="main-header">
+  document.getElementById("header").innerHTML = `  <div id="main-header">
             <nav class="mynavbar bg-light">
               <div class="container header">
                 <a class="brandLogo" href="index.html">
-                  <img src="logo" alt="your Logo">
+                  <div class="LogoIcon"></div>
                 </a>
                 <button class="navToggle">
                   <i class="fa fa-bars" aria-hidden="true"></i>
                 </button>
-                <ul class="navMenu mx-auto mb-2 mb-lg-0">
+                <ul class="navMenu ms-auto mb-2 mb-lg-0">
                   <li class="nav-close">
                     <button class="btn-nav-close"><span class="close-btn">+</span></button>
                   </li>
                   <li class="navLi"><a class="navLink" href="index.html">Home</a></li>
-                  <li class="navLi">
-                    <a href="#" class="navLink">About 
-                      <span class="toggleSub">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m6 10l6 6l6-6z"/></svg>
-                      </span>
-                    </a>
-                    <ul class="dropdownList">
-                      <li class="navLi"><a href="#" class="navLink">About Us</a></li>
-                      <li class="navLi"><a href="#" class="navLink">Meet Our Doctors</a></li>
-                    </ul>
-                  </li>
-                  <li class="navLi">
-                    <a href="#" class="navLink">Our Treatments 
-                      <span class="toggleSub">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m6 10l6 6l6-6z"/></svg>
-                      </span>
-                    </a>
-                    <ul class="dropdownList">
-                      <li class="navLi"><a href="#" class="navLink">LinkHere</a></li> 
-                    </ul>
-                  </li>
-                 
-                  <li class="navLi"><a href="#" class="navLink">Patient Testimonials</a></li>
-                  <li class="navLi"><a href="#" class="navLink">Gallery</a></li>
+                  <li class="navLi"><a href="#" class="navLink">Recent Work</a></li>
+                  <li class="navLi"><a href="#" class="navLink">Services</a></li>
+                  <li class="navLi"><a href="#" class="navLink">Our Work</a></li>
+                  <li class="navLi"><a href="#" class="navLink">About Us</a></li>
                   <li class="navLi"><a href="#" class="navLink">Contact</a></li>
                 </ul>
     
@@ -75,102 +38,13 @@ function loadHeaderFooter(callback) {
        
         </div>
 `; // Keep your current header HTML here
-  document.getElementById("footer").innerHTML = ` <div class="container">
-        <div class="footWrap defaultPadding">
-          <div class="row">
-            <!-- Company Info -->
-            <div class="col-lg-auto col-sm-6 mb-4 mb-md-0 FooterAbout">
-              <a class="footer-brand" href="index.html">
-                <img loading="lazy" src="image/Logo" alt="yourLogo">
-              </a>
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa recusandae quam at neque dolore nisi minima blanditiis fuga corporis architecto voluptatem provident dolores et vitae, vero 
-              </p>
-             
-            </div>
-            <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks">
-              <h5 class="fourthH">Quick Links</h5>
-              <ul>
-                <li>
-                  <a class="footerLinks" href="index.html">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Home </a>
-                </li>
-                <li>
-                  <a class="footerLinks" href="#">
-                    <i class="fa-solid fa-circle-chevron-right"></i>About Us</a>
-                </li>
-                <li>
-                  <a class="footerLinks" href="#">
-                    <i class="fa-solid fa-circle-chevron-right"></i>link</a>
-                </li>
-                <li>
-                  <a class="footerLinks" href="#">
-                    <i class="fa-solid fa-circle-chevron-right"></i>link</a>
-                </li>
-                <li>
-                  <a class="footerLinks" href="#">
-                    <i class="fa-solid fa-circle-chevron-right"></i>link</a>
-                </li>
-                <li>
-                  <a class="footerLinks" href="#">
-                    <i class="fa-solid fa-circle-chevron-right"></i>Contact</a>
-                </li>
-              </ul>
-            </div>
-            <div class="col-sm-6 col-lg-auto mb-4 mb-md-0 exploreLinks1">
-              <h5 class="fourthH ">Explore Link</h5>
-              <ul>   
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-                 <li><a href="#" class="footerLinks"><i class="fa-solid fa-circle-chevron-right"></i> link HERE</a></li> 
-              </ul>
-            </div>
-            <div class="col-md-6 col-lg-auto mb-4 mb-md-0">
-              <h5 class="fourthH ">Contact Us</h5>
-              <ul class="contact-info">
-                <li>
-                  <a class="locationLink footerLinks" target="_blank" href="#">
-                    <i class="fa-solid fa-location-dot" aria-hidden="true"></i>
-                    <A1-TF-25>
-                      <pre class="mb-0">
-2nd floor, Broadway Signature,
-Sevasi - Canal Rd, Sevasi,
-Vadodara, Gujarat 391101</pre>
-                  </a>
-                </li>
-                <li>
-                  <a href="tel:Number" class="footerLinks">
-                    <i class="fa-solid fa-phone"></i>Your Number</a>
-                </li>
-                <li>
-                  <a class="emailAnchor footerLinks" href="mailto:youremail">
-                    <i class="fa-solid fa-envelope" aria-hidden="true"></i>youremail</a>
-                </li>
-              </ul>
-              <div class="social-icons mt-3">
-                <a target="_blank" class="facebook" href="https://www.linkedin.com/">
-                  <i class="fa-brands fa-linkedin-in" aria-hidden="true"></i>
-                </a>
-                <a class="whatsapp set-url-target" rel="noopener" data-mobile-target="" data-desktop-target="_blank" target="_blank" href="https://api.whatsapp.com/send?phone=numberHere">
-                  <i class="fa-brands fa-whatsapp" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-          
-            <!-- Contact Info -->
-          </div>
-        </div>
-       
-      </div>
+  document.getElementById("footer").innerHTML = ` 
       <div class="f-bottom effect">
         <div class="container">
           <div class="row ">
             <div class="col-12">
               <div class="inner">
-                <div class="copyright text-center"> <span class="year"></span>  All Rights Reserved by CompanyName | Developed by <a href="https://shriiitrackingsolution.in/" target="_blank">
+                <div class="copyright text-center"> <span class="year"></span>  All Rights Reserved by LifeLensInterior | Developed by <a href="https://shriiitrackingsolution.in/" target="_blank">
                     <b>Shriii&nbsp;Tracking&nbsp;Solution</b>
                   </a>
                 </div>
@@ -245,8 +119,8 @@ function initMobileNavToggle() {
 
 // WhatsApp URL Adjuster (Device-based Detection)
 (function () {
-  const mobileLink = "https://api.whatsapp.com/send?phone=numberHere";
-  const desktopLink = "https://web.whatsapp.com/send?phone=numberHere";
+  const mobileLink = "https://api.whatsapp.com/send?phone=916354710794";
+  const desktopLink = "https://web.whatsapp.com/send?phone=916354710794";
 
   function isMobileDevice() {
     return /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
